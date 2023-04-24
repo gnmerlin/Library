73ca7ec39a1716c626721ae6346b04e68b5d6f23
Iustin <philiustin@gmail.com>
2023-04-25 02:03:36 +0300

    Task 001:Apply initial setup

---
+++ libraryPackage/Main.java
@@ -0,0 +1,13 @@
+package libraryPackage;
+
+public class Main  {
+
+	public static void main(String[] args) {
+		//problema:
+		//doua obiecte numere de tip intreg
+		//4 metode + - : *
+		//afisare in consola 
+
+	}
+
+}
